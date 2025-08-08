4abff7efc1f898cea190d8464e5a2a0e70e8213c
isgameliel <isgameliel@gmail.com>
2025-08-08 20:06:26 +0100

    eslint.config.mjs updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     rules: {
       '@typescript-eslint/no-this-alias': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
     },
   },
 ];
