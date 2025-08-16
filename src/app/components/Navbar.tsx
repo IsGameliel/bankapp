1655918ca54e642678ef58a53c888c26332e5a8d
isgameliel <isgameliel@gmail.com>
2025-08-16 17:37:00 +0100

    name change

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-2xl font-bold">
-              HuntingtonsBank
+              HuntingtonBank
             </Link>
           </div>
           <div className="hidden md:flex space-x-8">
